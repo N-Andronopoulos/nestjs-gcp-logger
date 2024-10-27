c18cda841ad2cd7cfa492a22c26b2343f544831a
Nikolas Andronopoulos <N-Andronopoulos@users.noreply.github.com>
2024-10-27 23:11:07 +0100

    Updated example a bit.

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -3,8 +3,13 @@ import { AppModule } from './app.module';
 import { GCPLoggerService } from '@tazgr/nestjs-gcp-logger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule.register(), { bufferLogs: true });
+  const app = await NestFactory.create(AppModule.register(), {
+    bufferLogs: true, // Starts writing when flushLogs is called.
+    abortOnError: true, // Propagates throws
+    logger: false, // So it doesn't use the normal console logger at all.
+  });
   app.useLogger(await app.resolve(GCPLoggerService));
+  app.flushLogs();
   await app.listen(3000);
 }
 
