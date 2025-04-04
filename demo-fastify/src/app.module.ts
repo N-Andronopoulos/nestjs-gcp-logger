9cc15604ed2de363a6952d91c1ab650472e4f4c4
Roman Vašek <rvasek0@gmail.com>
2025-04-04 15:34:59 +0200

    feat: Added fastify support and fixed some dev scripts

---
+++ demo-fastify/src/app.module.ts
@@ -0,0 +1,27 @@
+import { DynamicModule, Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { GCPLoggingModule } from '@tazgr/nestjs-gcp-logger';
+
+@Module({})
+export class AppModule {
+  static register(): DynamicModule {
+    return {
+      module: AppModule,
+      imports: [
+        GCPLoggingModule.register({
+          isGlobal: true,
+          projectId: 'default-project-id',
+          logName: 'my-app-api',
+          isAsyncLogger: false,
+          resourceType: 'global',
+          resourceLabels: {
+            my: 'label',
+          },
+        }),
+      ],
+      controllers: [AppController],
+      providers: [AppService],
+    };
+  }
+}
---
+++ test/express/app.e2e-spec.ts
@@ -0,0 +1,30 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../../demo-express/src/app.module';
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule.register()],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('/ (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/')
+      .set('Ip', '1.1.1.1')
+      .set('x-cloud-trace-context', 'trace1234/spanId1234;o=0')
+      .expect(200)
+      .expect('Hello World!');
+  });
+});
